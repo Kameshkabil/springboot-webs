a572910472a34687bcbb1330c4c91f25de2a58aa
kamesh <kameshkabil9@gmail.com>
2024-01-15 20:39:17 +0530

    view-all-posts using entitymanager

--- src/main/java/com/example/springbootwebs/controller/PostController.java
+++ src/main/java/com/example/springbootwebs/controller/PostController.java
@@ -7,10 +7,9 @@ import com.example.springbootwebs.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -21,6 +20,11 @@ public class PostController {
     public ResponseEntity<PostContent> createPostContent(@RequestBody PostRequest postRequest) {
         PostContent postContent = this.postService.createPostContent(postRequest);
         return new ResponseEntity<>(postContent, HttpStatus.CREATED);
+    }
+
+    @GetMapping("all/posts")
+    public List<PostContent> getallPostsContent(){
+        return postService.getallPosts();
     }
 
 }
