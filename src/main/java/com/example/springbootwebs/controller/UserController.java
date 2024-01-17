a4f9f27208ab1930c65555d7b2869ccedd661ed8
kamesh <kameshkabil9@gmail.com>
2024-01-17 15:57:55 +0530

    Added Feature for view-all-users successfully

--- src/main/java/com/example/springbootwebs/controller/UserController.java
+++ src/main/java/com/example/springbootwebs/controller/UserController.java
@@ -2,9 +2,11 @@ package com.example.springbootwebs.controller;
 
 import com.example.springbootwebs.model.User;
 import com.example.springbootwebs.service.UserService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -31,5 +33,10 @@ public class UserController {
     public User upadateUserprofile(@RequestBody User user,@PathVariable(value = "id")
                                    long id){
         return userService.updateUserProfiles(user,id);
+    }
+
+    @GetMapping("/view-all-users")
+    public List<User> getAllUsers(){
+        return userService.getAllUsers();
     }
 }
